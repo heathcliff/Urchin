58af93136a8804d47b92e6b5468bb751ff7debad
Heath Beckett <heath@asheavenue.com>
2014-03-01 20:05:32 -0500

    adding Utility::timeAgo()

--- classes/Utility.php
+++ classes/Utility.php
@@ -18,5 +18,32 @@ class Utility {
         return $text;
     }
 
+    public static function timeAgo($date, $granularity = 1) {
+        $difference = time() - $date;
+        $periods = array(
+            'decade' => 315360000,
+            'year'   => 31536000,
+            'month'  => 2628000,
+            'week'   => 604800,
+            'day'    => 86400,
+            'hour'   => 3600,
+            'minute' => 60,
+            'second' => 1
+        );
+        $retval = '';
+        foreach ($periods as $key => $value) {
+            if ($difference >= $value) {
+                $time = floor($difference/$value);
+                $difference %= $value;
+                $retval .= ($retval ? ' ' : '').$time.' ';
+                $retval .= (($time > 1) ? $key.'s' : $key);
+                $granularity--;
+            }
+            if ($granularity == '0') {
+                break;
+            }
+        }
+        return ' posted ' . $retval . ' ago';
+    }
 
 }
